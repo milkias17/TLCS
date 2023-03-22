d9d86090d48cd42f0abbeaebcd28f976e5d06624
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-03-23 00:16:15 +0300

    replace placeholder text on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import UserContext from "@/context/UserContext";
+import Link from "next/link";
+import { useContext } from "react";
 import HeroImage from "../../public/hero.jpg";
 
 export default function Home() {
+  const { user } = useContext(UserContext);
   return (
     <>
       <div
@@ -9,19 +13,21 @@ export default function Home() {
           backgroundImage: `url(${HeroImage.src})`,
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
-          backgroundSize: "cover"
+          backgroundSize: "cover",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">
+              {user ? `Hello there ${user?.role}` : "Hello there!"}
+            </h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              {user
+                ? "Welcome to TLCS, the ultimate teaching learning collaborating system, checkout your dashboard to see what functions you have."
+                : "Login to use TLCS"}
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <Link className="btn btn-primary" href={user ? `/${user.role.toLowerCase()}` : "/login/student"}>Get Started</Link>
           </div>
         </div>
       </div>
