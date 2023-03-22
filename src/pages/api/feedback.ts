30b0661574ed1984d66a2dbb6480e144ea2d8cb7
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-03-22 16:20:10 +0300

    fix giveFeedback page

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,29 +1,31 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function handleFeedback(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method === 'POST') {
-        try {
-            const { user_id, course_code, rating, comment, no_chapter_completed } = req.body;
-            const feedback = await prisma.feedback.create({
-                data: {
-                    user_id,
-                    course_code,
-                    rating,
-                    comment,
-                    no_chapter_completed,
-                },
-            });
-            res.status(200).json({ detail: " Your feedback Successfullly submitted" });
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to sent feedback' });
-        }
-    } else {
-        res.status(400).json({ error: 'Invalid request method' });
+  if (req.method === "POST") {
+    try {
+      const { user_id, course_code, rating, comment, no_chapter_completed } =
+        JSON.parse(req.body);
+      const feedback = await prisma.feedback.create({
+        data: {
+          user_id,
+          course_code,
+          rating: parseInt(rating),
+          comment,
+          no_chapter_completed: parseInt(no_chapter_completed),
+        },
+      });
+      res.status(200).redirect("/");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to sent feedback" });
     }
+  } else {
+    res.status(400).json({ error: "Invalid request method" });
+  }
 }
--- src/pages/student/giveFeedback.tsx
+++ src/pages/student/giveFeedback.tsx
@@ -2,112 +2,207 @@ import SidebarContent from "../../components/SidebarContent";
 import { feedback } from "@prisma/client";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { makePostRequest } from "../../lib/apiClient";
 import { Course } from "@prisma/client";
-import Prisma from "../../lib/prisma";
-import { getSession } from "../../lib/session";// @Milikiyas Please implement this part to get the user_id from the session of your auth
+import prisma from "../../lib/prisma";
+import UserContext from "../../context/UserContext";
 
 type Props = {
-    user_id: string[];
-    courses: Course[];
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => { // get courses from Enrolment
-    const session = await getSession(context);
-    const user_id = session?.user?.id;
-    const user = await Prisma.user.findUnique({
-        where: user_id,
-        include: { Course: true },
-    });
-    const courses = user?.Course || [];
-
-    return {
-        props: {
-            user_id,
-            courses,
-        },
-    };
+  user_id: string[];
+  courses: Course[];
 };
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // get courses from Enrolment
+  const uuid = context.req.cookies.sessionId;
+  const session = await prisma.session.findUnique({
+    where: {
+      uuid,
+    },
+    include: {
+      user: true,
+    },
+  });
 
-
-const FeedbackForm = ({ courses, user_id }: Props) => {
-    const [course_code, setCourse_code] = useState('');
-    const [rating, setRating] = useState('');
-    const [comment, setComment] = useState('');
-    const [no_chapter_completed, setNo_chapter_completed] = useState('');
-    const [errorMsg, setErrorMsg] = useState("");
-    const router = useRouter();
-
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
-        const error = await makePostRequest('/api/feedback', {
-            user_id,
-            course_code,
-            rating,
-            comment,
-            no_chapter_completed,
-        });
-        if (error) {
-            setErrorMsg(error)
-        }
-        alert('Feedback created successfully!');
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
     };
+  }
 
-    return (
-        <>
-            <SidebarContent>
-                <form onSubmit={handleSubmit} method="post"
-                    className="form-control gap-4">
-                    {errorMsg && (
-                        <h1 className="text-center text-3xl text-red-500 my-4">
-                            {errorMsg}
-                        </h1>
-                    )}
-                    <label htmlFor="course_code" className="input-group">
-                        <span className="hidden w-1/2 sm:inline-flex" >Select Course:</span>
-                        <select
-                            className="select w-full max-w-xs self-center select-bordered"
-                            value={course_code}
-                            onChange={(e) => setCourse_code(e.target.value)}
-                            required
-                        >
-                            <option value="">Select course</option>
-                            {courses.map((crs) => (
-                                <option key={crs.course_code} value={crs.course_code}>
-                                    {crs.course_name}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <label htmlFor="rating" className="input-group">
-                        <span className="hidden w-1 /2 sm: inline-flex">Rating:</span>
-                        <select value={rating} onChange={(e) => setRating(e.target.value)} required>
-                            <option value="">Select rating</option>
-                            <option value="1">1 star</option>
-                            <option value="2">2 stars</option>
-                            <option value="3">3 stars</option>
-                            <option value="4">4 stars</option>
-                            <option value="5">5 stars</option>
-                        </select>
-                    </label>
-                    <label htmlFor="comment" className="input-group">
-                        <span className="hidden w-1/2 sm:inline-flex">Comment:</span>
-                        <textarea className="textarea-primary" value={comment} onChange={(e) => setComment(e.target.value)} />
-                    </label>
-                    <label htmlFor="rating" className="input-group">
-                        <span className="hidden w-1/2 sm:inline-flex">No. of Chapters Completed:</span>
-                        <input type="number" value={no_chapter_completed} onChange={(e) => setNo_chapter_completed(e.target.value)} />
-                    </label>
-                    <button type="submit" className="btn-primary btn">
-                        Submit Feedback
-                    </button>
-                </form>
-            </SidebarContent>
+  const user = session?.user;
+  const enrolments = await prisma.enrolment.findMany({
+    where: {
+      student_id: user.user_id,
+    },
+    include: {
+      course: true,
+    },
+  });
+  let courses: Course[];
 
-        </>
+  if (enrolments) {
+    courses = enrolments.map((enrolment) => enrolment.course);
+  } else {
+    courses = [];
+  }
+
+  return {
+    props: {
+      courses,
+    },
+  };
+};
+
+const FeedbackForm = ({ courses }: Props) => {
+  const [course_code, setCourse_code] = useState("");
+  const [rating, setRating] = useState(2);
+  const [comment, setComment] = useState("");
+  const [no_chapter_completed, setNo_chapter_completed] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+  const { user } = useContext(UserContext);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    const error = await makePostRequest(
+      "/api/feedback",
+      {
+        user_id: user?.user_id,
+        course_code,
+        rating,
+        comment,
+        no_chapter_completed,
+      },
+      router,
+      true
     );
+
+    if (error) {
+      setErrorMsg(error);
+    } else {
+      alert("Feedback created successfully!");
+    }
+  };
+
+  return (
+    <>
+      <SidebarContent>
+        <form
+          onSubmit={handleSubmit}
+          method="post"
+          className="form-control gap-4"
+        >
+          {errorMsg && (
+            <h1 className="text-center text-3xl text-red-500 my-4">
+              {errorMsg}
+            </h1>
+          )}
+          <label htmlFor="course_code" className="input-group">
+            <span className="hidden w-1/2 sm:inline-flex">Select Course:</span>
+            <select
+              className="select w-full max-w-xs self-center select-bordered"
+              value={course_code}
+              onChange={(e) => setCourse_code(e.target.value)}
+              required
+            >
+              <option value="">Select course</option>
+              {courses.map((crs) => (
+                <option key={crs.course_code} value={crs.course_code}>
+                  {crs.course_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          {/* <label htmlFor="rating" className="input-group"> */}
+          {/*   <span className="hidden w-1 /2 sm:inline-flex">Rating:</span> */}
+          {/*   <select */}
+          {/*     value={rating} */}
+          {/*     onChange={(e) => setRating(e.target.value)} */}
+          {/*     required */}
+          {/*   > */}
+          {/*     <option value="">Select rating</option> */}
+          {/*     <option value="1">1 star</option> */}
+          {/*     <option value="2">2 stars</option> */}
+          {/*     <option value="3">3 stars</option> */}
+          {/*     <option value="4">4 stars</option> */}
+          {/*     <option value="5">5 stars</option> */}
+          {/*   </select> */}
+          {/* </label> */}
+          <div className="rating">
+            <label htmlFor="rating-1" className="mr-10">
+              Rating
+            </label>
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              value={1}
+              onClick={(e) => setRating(1)}
+              checked={rating === 1}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              value={2}
+              checked={rating === 2}
+              onClick={(e) => setRating(2)}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              value={3}
+              checked={rating === 3}
+              onClick={(e) => setRating(3)}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              value={4}
+              checked={rating === 4}
+              onClick={(e) => setRating(4)}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              value={5}
+              checked={rating === 5}
+              onClick={(e) => setRating(5)}
+            />
+          </div>
+          <label htmlFor="comment" className="input-group">
+            <span className="hidden w-1/2 sm:inline-flex">Comment:</span>
+            <textarea
+              className="textarea-primary"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </label>
+          <label htmlFor="rating" className="input-group">
+            <span className="hidden w-1/2 sm:inline-flex">
+              No. of Chapters Completed:
+            </span>
+            <input
+              type="number"
+              value={no_chapter_completed}
+              onChange={(e) => setNo_chapter_completed(e.target.value)}
+            />
+          </label>
+          <button type="submit" className="btn-primary btn">
+            Submit Feedback
+          </button>
+        </form>
+      </SidebarContent>
+    </>
+  );
 };
 
 export default FeedbackForm;
