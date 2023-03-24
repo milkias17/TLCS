eaa4e72cbf59e142c48c57faccb2830888a02a0f
Keku Teresa <kekubt@gmail.com>
2023-03-24 03:01:04 -0400

    UI Improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,16 +18,21 @@ export default function Home() {
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-              {user ? `Hello there ${user?.role}` : "Hello there!"}
+          <div className="max-w-md mx-auto">
+            <h1 className="mb-5 text-5xl font-bold text-primary tracking-wide">
+              {user ? `Hello there, ${user?.role}!` : "Hello there!"}
             </h1>
-            <p className="mb-5">
+            <p className="mb-8 text-lg text-gray-600">
               {user
                 ? "Welcome to TLCS, the ultimate teaching learning collaborating system, checkout your dashboard to see what functions you have."
                 : "Login to use TLCS"}
             </p>
-            <Link className="btn btn-primary" href={user ? `/${user.role.toLowerCase()}` : "/login/student"}>Get Started</Link>
+            <Link
+              className="btn btn-primary px-8 py-3 rounded-full text-xl font-bold shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out"
+              href={user ? `/${user.role.toLowerCase()}` : "/login/student"}
+            >
+              {user ? `Get Started` : `Log In`}
+            </Link>
           </div>
         </div>
       </div>
